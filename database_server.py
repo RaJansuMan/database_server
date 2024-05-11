06685114ba1d2c9a5e5f8b8fa8c527995f0cfc60
RaJansuMan <rajansuman180@gmail.com>
2024-05-11 21:01:32 +0530

    read protocol done

--- database_server.py
+++ database_server.py
@@ -5,6 +5,7 @@ from collections import namedtuple
 from io import BytesIO
 from socket import error as socket_error
 
+
 # Data-type	       Prefix	Structure	                  Example
 # Simple string    	+	 +{string data}\r\n	               +this is a simple string\r\n
 
@@ -55,62 +56,76 @@ class ProtocolHandler(object):
             '$': self.handle_string,
             '*': self.handle_array,
             '%': self.handle_dict}
+
     def handle_request(self, socket_file):
         first_byte = socket_file.read(1)
         if not first_byte:
             raise Disconnect
         try:
             return self.handlers[first_byte](socket_file)
-        except KeyError :
+        except KeyError:
             raise CommandError('bad request')
 
-    def handle_simple_string(self,socket_file):
+    def handle_simple_string(self, socket_file):
         return socket_file.readline().rstrip('\r\n')
 
-    def handle_error(self,socket_file):
+    def handle_error(self, socket_file):
         return Error(socket_file.readline().rstrip('\r\n'))
 
-    def handle_integer(self,socket_file):
+    def handle_integer(self, socket_file):
         return int(socket_file.readline().rstrip('\r\n'))
 
-    def handle_string(self,socket_file):
+    def handle_string(self, socket_file):
         length = int(socket_file.readline().rstrip('\r\n'))
         if length == -1:
             return None
-        length +=2
+        length += 2
         return socket_file.read(length)[:-2]
 
-    def handle_array(self,socket_file):
-        num_elements=int(socket_file.readline().rstrip('\r\n'))
+    def handle_array(self, socket_file):
+        num_elements = int(socket_file.readline().rstrip('\r\n'))
         return [self.handle_request(socket_file) for _ in range(num_elements)]
 
-    def handle_dict(self,socket_file):
-        num_items=int(socket_file.readline().rstrip('\r\n'))
+    def handle_dict(self, socket_file):
+        num_items = int(socket_file.readline().rstrip('\r\n'))
         elements = [self.handle_request(socket_file)
-                    for _ in range (num_items*2)]
-        return dict(zip(elements[::2],elements[1::2]))
+                    for _ in range(num_items * 2)]
+        return dict(zip(elements[::2], elements[1::2]))
 
     def write_response(self, socket_file, data):
-        pass
+        buf = BytesIO
+        self._write(buf, data)
+        buf.seek(0)
+        socket_file.write(buf.getvalue())
+        socket_file.flush()
+
+    def _write(self, buf, data):
+        if isinstance(data, str):
+            data = data.encode('utf-8')
+            buf.write('+%s\r\n' % data)
+        if isinstance(data, bytes):
+            buf.write(':%s\r\n%s\r\n' % (len(data), data))
+        if isinstance(data,Error):
+            buf.write()
 
 
 class Server(object):
     def __init__(self, host='127.0.0.1', port=31337, max_clients=64):
         self._pool = Pool(max_clients)
         self._server = StreamServer(
-            (host,port),
+            (host, port),
             self.connection_handler,
             spawn=self.pool
         )
         self._protocol = ProtocolHandler()
         self._kv = {}
 
-    def connection_handler(self,conn,address):
+    def connection_handler(self, conn, address):
         socket_file = conn.makefile('rwb')
 
         while True:
             try:
-                data= self._protocol.handle_request(socket_file)
+                data = self._protocol.handle_request(socket_file)
             except Disconnect:
                 break
 
